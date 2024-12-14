86dd40befe2b4e1617d0c07987164ca5b1f2d146
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2024-12-14 20:49:48 -0300

    feat: initial development of the service

---
+++ weather-app/src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey = 'e48df9e4f5dc37765c7c61921d8f283c';
+
+  constructor(private angular_http: HttpClient) {}
+
+  getWeatherDatas(cityName: String): Observable<any>
+  {
+    return this.angular_http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}`, {});
+  }
+}
