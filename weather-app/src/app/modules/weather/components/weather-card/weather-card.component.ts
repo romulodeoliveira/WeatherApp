3d27c920288b83aba273eaafe23a7c28bbe32ecd
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2024-12-15 12:13:28 -0300

    feat: implementing the weather card component

---
+++ weather-app/src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherInterface } from 'src/app/models/interfaces/weather.interface';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent {
+
+  @Input() weatherInterfaceInput!: WeatherInterface;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+}
